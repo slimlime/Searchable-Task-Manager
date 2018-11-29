d8157573d122fe271fe7a45c0ae336a22a59a807
unknown <davidriddell13@gmail.com>
2018-11-29 10:25:39 +1000

    snapshot of processes no errors

--- SearchableTaskManager/SearchableTaskManager.cpp
+++ SearchableTaskManager/SearchableTaskManager.cpp
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include <tlhelp32.h>
 #include <tchar.h>
+#include <stdio.h>
 
 #define MAX_LOADSTRING 100
 
@@ -16,13 +17,13 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name// 
 BOOL GetProcessList();
 BOOL ListProcessModules(DWORD dwPID);
 BOOL ListProcessThreads(DWORD dwOwnerPID);
-void printError(TCHAR* msg);
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -186,7 +187,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	return (INT_PTR)FALSE;
 }
 
-BOOL getProccessList() {
+BOOL GetProcessList() {
 	/*
 	* Handle: is a "abstract pointer" that is not meant to be treated as a pointer, used as an identifier
 	* useless unless passed through an api
@@ -202,6 +203,83 @@ BOOL getProccessList() {
 	//Take snapshot of computer processes
 	processSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	if (processSnap == INVALID_HANDLE_VALUE) {
-		printError("CreateToolHelp32Snapshot (of processes)");
+		printf("CreateToolHelp32Snapshot (of processes)");
 	}
 }
+
+BOOL ListProcessModules(DWORD dwPID) {
+	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+	MODULEENTRY32 me32;
+
+	// Take a snapshot of all modules in the specified process.
+	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+	if (hModuleSnap == INVALID_HANDLE_VALUE) {
+		printf("CreateToolhelp32Snapshot (of modules)");
+		return(FALSE);
+	}
+
+	// Set the size of the structure before using it.
+	me32.dwSize = sizeof(MODULEENTRY32);
+
+	// Retrieve information about the first module,
+	// and exit if unsuccessful
+	if (!Module32First(hModuleSnap, &me32)) {
+		printf("Module32First");  // show cause of failure
+		CloseHandle(hModuleSnap);           // clean the snapshot object
+		return(FALSE);
+	}
+
+	// Now walk the module list of the process,
+	// and display information about each module
+	do {
+		_tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
+		_tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
+		_tprintf(TEXT("\n     Process ID     = 0x%08X"), me32.th32ProcessID);
+		_tprintf(TEXT("\n     Ref count (g)  = 0x%04X"), me32.GlblcntUsage);
+		_tprintf(TEXT("\n     Ref count (p)  = 0x%04X"), me32.ProccntUsage);
+		_tprintf(TEXT("\n     Base address   = 0x%08X"), (DWORD)me32.modBaseAddr);
+		_tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
+
+	} while (Module32Next(hModuleSnap, &me32));
+
+	CloseHandle(hModuleSnap);
+	return(TRUE);
+}
+
+BOOL ListProcessThreads(DWORD dwOwnerPID) {
+	HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
+	THREADENTRY32 te32;
+
+	// Take a snapshot of all running threads  
+	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (hThreadSnap == INVALID_HANDLE_VALUE)
+		return(FALSE);
+
+	// Fill in the size of the structure before using it. 
+	te32.dwSize = sizeof(THREADENTRY32);
+
+	// Retrieve information about the first thread,
+	// and exit if unsuccessful
+	if (!Thread32First(hThreadSnap, &te32)) {
+		printf("Thread32First"); // show cause of failure
+		CloseHandle(hThreadSnap);          // clean the snapshot object
+		return(FALSE);
+	}
+
+	// Now walk the thread list of the system,
+	// and display information about each thread
+	// associated with the specified process
+	do {
+		if (te32.th32OwnerProcessID == dwOwnerPID) {
+			_tprintf(TEXT("\n\n     THREAD ID      = 0x%08X"), te32.th32ThreadID);
+			_tprintf(TEXT("\n     Base priority  = %d"), te32.tpBasePri);
+			_tprintf(TEXT("\n     Delta priority = %d"), te32.tpDeltaPri);
+			_tprintf(TEXT("\n"));
+		}
+	} while (Thread32Next(hThreadSnap, &te32));
+
+	CloseHandle(hThreadSnap);
+	return(TRUE);
+}
+
+
